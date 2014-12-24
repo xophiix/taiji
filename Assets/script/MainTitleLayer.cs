0212c592e2464dc613e146386ef20956ab74a1fe
xophiix <xophiix@gmail.com>
2014-12-25 03:16:27 +0800

    add game score title layer transit animation

--- Assets/script/MainTitleLayer.cs
+++ Assets/script/MainTitleLayer.cs
@@ -8,46 +8,20 @@ public class MainTitleLayer : MonoBehaviour {
 	public Color whiteScoreColor = Color.white;
 	public Color blackScoreTextColor = Color.black;
 	public Color whiteScoreTextColor = Color.white;
-
-	private MainState.PawnType _scorePawnType = MainState.PawnType.Black;
-	private Image _titleImage;
-	private List<Text> _textList = new List<Text>();
+	private Animator _animator;
 
 	void Awake() {
-		_titleImage = gameObject.GetComponent<Image>();
-		_textList.Add(gameObject.transform.Find("Level").GetComponent<Text>());
-		_textList.Add(gameObject.transform.Find("Trash/Count").GetComponent<Text>());
-		_textList.Add(gameObject.transform.Find("Backwards/Count").GetComponent<Text>());
-
-		foreach (Text text in _textList) {
-			text.color = whiteScoreTextColor;
-		}
-	}
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		_animator = gameObject.GetComponent<Animator>();
 	}
 
 	public void setScorePawnType(MainState.PawnType type) {
-		Debug.Log("setScorePawnType");
-		_scorePawnType = type;
-		Color textColor;
-		if (type == MainState.PawnType.Black) {
-			_titleImage.color = blackScoreColor;
-			textColor = blackScoreTextColor;
-		} else {
-			_titleImage.color = whiteScoreColor;
-			textColor = whiteScoreTextColor;
-		}
+		_animator.ResetTrigger("Black");
+		_animator.ResetTrigger("White");
 
-		foreach (Text text in _textList) {
-			text.color = textColor;
+		if (type == MainState.PawnType.Black) {
+			_animator.SetTrigger("Black");
+		} else {
+			_animator.SetTrigger("White");
 		}
 	}
 }
---
+++ Assets/script/ScreenBase.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenBase : MonoBehaviour {
+	public string openAnim;
+	public string closeAnim;
+	public string normalState;
+
+	private Animator _screenAnimator;
+	private string _curOpenAnim;
+	private string _curCloseAnim;
+
+	public Animator screenAnimator {
+		get { return _screenAnimator; }
+	}
+
+	protected void Awake () {
+		if (openAnim == "") {
+			openAnim = "ScreenIn";
+		}
+
+		if (closeAnim == "") {
+			closeAnim = "ScreenOut";
+		}
+
+		if (normalState == "") {
+			normalState = "Normal";
+		}
+
+		_curOpenAnim = openAnim;
+		_curCloseAnim = closeAnim;
+
+		_screenAnimator = gameObject.GetComponent<Animator>();
+		ScreenManager.instance().registerScreen(gameObject.name, gameObject);
+	}
+
+	void OnDestroy() {
+		ScreenManager.instance().unregisterScreen(gameObject.name);
+	}
+
+	public virtual void onShow(bool show) {
+
+	}
+
+	protected virtual void onScreenIn() {
+		
+	}
+
+	protected virtual void onScreenOut() {
+		
+	}
+	
+	protected void Update () {
+	}
+
+	public void open(string anim = "", bool directly = false) {
+		Debug.Log("open screen " + gameObject.name + " " + Time.time);
+		gameObject.SetActive(true);
+		gameObject.GetComponent<RectTransform>().localPosition = Vector3.zero;
+		//gameObject.transform.SetAsLastSibling();
+		if (_screenAnimator != null && !directly) {
+			if (anim == "") {
+				anim = openAnim;
+			}
+
+			_curOpenAnim = anim;
+			_screenAnimator.SetTrigger(_curOpenAnim);
+			StartCoroutine("startCheckOpen");
+		} else {
+			onOpened();
+		}
+	}
+
+	private void onOpened() {
+		Debug.Log("open screen done " + gameObject.name);
+		onScreenIn();
+		gameObject.SendMessageUpwards("onScreenIn", this);
+	}
+
+	IEnumerator startCheckOpen() {
+		yield return new WaitForEndOfFrame();
+		StartCoroutine(checkOpened());
+	}
+
+	IEnumerator checkOpened() {
+		bool opened = false;
+		while (!opened) {
+			opened = _screenAnimator.GetCurrentAnimatorStateInfo(0).IsName(normalState);
+			if (opened) {
+				Debug.Log("opened on checkOpened " + gameObject.name);
+				break;
+			}
+
+			yield return null;
+		}
+
+		onOpened();
+	}
+
+	public void close(string anim = "", bool directly = false) {
+		Debug.Log("close screen " + gameObject.name + " " + Time.time);
+		if (_screenAnimator != null && !directly) {
+			if (anim == "") {
+				anim = closeAnim;
+			}
+
+			_curCloseAnim = anim;
+			_screenAnimator.SetTrigger(_curCloseAnim);
+			StartCoroutine("startCheckClose");
+		} else {
+			onClosed();
+		}
+	}
+
+	private void onClosed() {
+		Debug.Log("close screen done " + gameObject.name + " " + Time.time);
+		onScreenOut();
+		gameObject.SendMessageUpwards("onScreenOut", this);
+		gameObject.SetActive(false);
+	}
+
+	IEnumerator startCheckClose() {
+		yield return new WaitForEndOfFrame();
+		StartCoroutine(checkClosed());
+	}
+
+	IEnumerator checkClosed() {
+		bool closed = false;
+		while (!closed) {
+			AnimatorStateInfo stateInfo = _screenAnimator.GetCurrentAnimatorStateInfo(0);
+			closed = (stateInfo.IsName(_curCloseAnim) && stateInfo.normalizedTime >= 0.95f) || stateInfo.IsName(normalState);
+			if (closed) {
+				Debug.Log("close on checkClosed " + gameObject.name);
+				break;
+			}
+
+			yield return null;
+		}
+
+		onClosed();
+	}
+}
